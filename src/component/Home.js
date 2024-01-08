638ebb3fa94e2ed0db93911ffd995e4fbcf12242
samiksha0700 <114768682+samiksha0700@users.noreply.github.com>
2024-01-08 15:37:21 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,37 +1,40 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './Header';
 import RestaurantsDisplay from './Restaurants';
 
 const url = "http://43.205.216.77:8443/restaurant/";
 
-class Home extends Component{
-    constructor(){
-        super()
+class Home extends Component {
+    constructor() {
+        super();
 
-        this.state={
-            restaurant:''
-        }
+        this.state = {
+            restaurants: [] // Initialize as an array
+        };
     }
 
-    componentDidMount(){
-        fetch(url,{
-            method:'GET'
+    componentDidMount() {
+        fetch(url, {
+            method: 'GET'
         })
-        .then((res) => res.json())
-        .then((data) => {
-            this.setState({
-                restaurant:data
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    restaurants: Array.isArray(data) ? data : [] // Update state with an array
+                });
             })
-        })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <Fragment>
-                <Header/>
-                <RestaurantsDisplay datalist={this.state.restaurant}/>
+                <Header />
+                <RestaurantsDisplay datalist={this.state.restaurants} />
             </Fragment>
-        )
+        );
     }
 }
 
